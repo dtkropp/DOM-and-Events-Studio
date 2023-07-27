e79612572dfa05d8b67699591ff923c22548ee87
dtkropp <dtkropp@gmail.com>
2023-07-27 13:20:48 -0500

    finished assignment

--- scripts.js
+++ scripts.js
@@ -1,50 +1,140 @@
-// Write your JavaScript code here.
-// Remember to pay attention to page loading!
-function init(){
+
+function init() {
     const takeOffButton = document.getElementById("takeoff");
-    const flightStatus = document.getElementById("flightstatus");
-    let shuttleHeight = Number(document.getElementById)
-    const landButton = document.getElementById("landing");
-    const abortButton = document.getElementById("missionAbort");
-    const down = document.getElementById("down");
-    const rocketImg = document.getElementById("rocket");
+    const flightStatus= document.getElementById("flightStatus");
+    let shuttleHeight= Number(document.getElementById("spaceShuttleHeight").innerHTML);
+    const landButton= document.getElementById("landing");
+    const abortButton= document.getElementById("missionAbort");
+    const downB= document.getElementById("down");
+    const leftB= document.getElementById("left");
+    const rightB= document.getElementById("right");
+    const upB= document.getElementById("up");
+    const rocketImg= document.getElementById("rocket");
     
-    rocketImg.style.left = '5px'
-    rocketImg.style.right = '35px'
-    rocketImg.style.bottom= '35px'
-    rocketImg.style.top = '250px’
+    
+    let top =250;
+    let bottom =0;
+    let left =250;
+    let right =250;
+    rocketImg.style.bottom =bottom+'px';
+    rocketImg.style.left =left+'px';
+    rocketImg.style.right =right+'px'; 
+    rocketImg.style.top =top+'px';
+  
+    takeOffButton.addEventListener("click",function (event) {
 
-    takeOffButton.addEventListener("click", function (event) {
-        if(confirm("Confirm that the shuttle is ready for takeoff.")) {
-            flightStatus.innerHTML = "Shuttle in flight.";
-            document.getElementById("shuttlebackground").style.backgroundColor = 'blue';
-            shuttleHeight += 10000;
-            document.getElementById("spaceShuttleHeight").innerHTML = shuttleHeight;
-
+    if(confirm("Confirm that the shuttle is ready for takeoff.")) {
+        flightStatus.innerHTML= "Shuttle in flight.";   
+        document.getElementById("shuttleBackground").style.backgroundColor = 'blue';
+        shuttleHeight += 10000;
+        document.getElementById("spaceShuttleHeight").innerHTML=shuttleHeight;
+        top -=10;
+        rocketImg.style.top =top+'px';
         }
     });
-    landButton.addEventListener("click", function (event) {
-        window.alert("The shuttle is landing. Landing gear engaged.")
-            flightStatus.innerHTML = "Shuttle has landed.";
-            document.getElementById("shuttlebackground").style.backgroundColor = 'green';
-            shuttleHeight += 0;
-            document.getElementById("spaceShuttleHeight").innerHTML = shuttleHeight; 
+       
+    landButton.addEventListener("click",function (event) {
+
+    window.alert("The shuttle is landing. Landing gear engaged");
+        flightStatus.innerHTML= "Shuttle has landed.";   
+        document.getElementById("shuttleBackground").style.backgroundColor = 'green';
+        shuttleHeight = 0;
+        document.getElementById("spaceShuttleHeight").innerHTML=shuttleHeight;
+        top =250;
+        bottom =0;
+        left =250;
+        right =250;
+        rocketImg.style.bottom =bottom+'px';
+        rocketImg.style.left =left+'px';
+        rocketImg.style.right =right+'px'; 
+        rocketImg.style.top =top+'px';git
         
     });
+        
+    abortButton.addEventListener("click",function (event) {
 
+    if(confirm("Confirm that you want to abort the mission.")) {
+        flightStatus.innerHTML= "Mission aborted.";   
+        document.getElementById("shuttleBackground").style.backgroundColor = 'green';
+        shuttleHeight = 0;
+        document.getElementById("spaceShuttleHeight").innerHTML=shuttleHeight;
+        top =250;
+        bottom =0;
+        left =250;
+        right =250;
+        rocketImg.style.bottom =bottom+'px';
+        rocketImg.style.left =left+'px';
+        rocketImg.style.right =right+'px'; 
+        rocketImg.style.top =top+'px';
+          
+        }
+    });
     
-    abortButton.addEventListener("click", function (event) {
-         if(confirm("Confirm that you want to abort the mission.")) {
-            flightStatus.innerHTML = "Mission aborted.";
-            document.getElementById("shuttlebackground").style.backgroundColor = 'green';
-            shuttleHeight += 0;
-            document.getElementById("spaceShuttleHeight").innerHTML = shuttleHeight;
+
+    downB.addEventListener("click", function(event){
+       
+    if(shuttleHeight==0){
+        document.getElementById("shuttleBackground").style.backgroundColor = 'green';
+        flightStatus.innerHTML= "Shuttle has landed.";   
+        document.getElementById("spaceShuttleHeight").innerHTML=shuttleHeight;
+           
+    }else{
+        top+=10;
+        rocketImg.style.top =top+'px';
+        shuttleHeight-=10000;
+        flightStatus.innerHTML= "Shuttle in flight."; 
+        document.getElementById("spaceShuttleHeight").innerHTML=shuttleHeight;
         }
     });
 
-    down.addEventListener("click", function(event){
-        let position = parsInt(rocketImg.style.bottom);
-        position -=10 ;
+    upB.addEventListener("click", function(event){
+       
+    if(top==0){
+        flightStatus.innerHTML= "Maximum shuttle height reached.";   
+        document.getElementById("spaceShuttleHeight").innerHTML=shuttleHeight;
+       
+    }else{
+        document.getElementById("shuttleBackground").style.backgroundColor = 'blue';
+        top-=10;
+        rocketImg.style.top =top+'px';
+        shuttleHeight+=10000;
+        flightStatus.innerHTML= "Shuttle in flight"; 
+        document.getElementById("spaceShuttleHeight").innerHTML=shuttleHeight;
 
-        rocketImg.style.bottom = ;
-    })
+    if(top==250)
+        {
+        }
+     }
+
+    });
+
+    leftB.addEventListener("click", function(event){
+       
+    if(left==0){
+       flightStatus.innerHTML= "Maximum left movement reached.";   
+    }else{
+        left-=10;
+        rocketImg.style.left =left+'px';
+        document.getElementById("spaceShuttleHeight").innerHTML=shuttleHeight;
+    }
+
+    });
+
+    rightB.addEventListener("click", function(event){
+       
+    if(left==550){
+        flightStatus.innerHTML= "Maximum right movement reached.";   
+       
+    }else{
+   
+        left+=10;
+        rocketImg.style.left =left+'px';
+        document.getElementById("spaceShuttleHeight").innerHTML=shuttleHeight;
+    }
+
+
+    });
+
+    }
+
+window.addEventListener("load", init);
